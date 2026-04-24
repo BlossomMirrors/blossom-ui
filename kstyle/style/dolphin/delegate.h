ec61941586af4b1ef3e05f9ddb38c0364db6c90a
vaporvee <yannik@vaporvee.com>
2026-04-24 19:54:10 +0200

    missing import

--- kstyle/style/dolphin/delegate.h
+++ kstyle/style/dolphin/delegate.h
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include "blossomuistyleconfigdata.h"
 
+#include <cmath>
 #include <QAbstractItemDelegate>
 #include <QAbstractItemView>
 #include <QApplication>
