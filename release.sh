8427a8fb3e4f73c0e5815366da7c5c14c151bfea
vaporvee <yannik@vaporvee.com>
2026-09-17 17:51:18 +0200

    fixed corners FINALLY

--- release.sh
+++ release.sh
@@ -110,6 +110,7 @@ fi
 \$KWRITE --file /etc/xdg/kdeglobals --group General --key LightColorScheme 'BlossomUI Light'
 \$KWRITE --file /etc/xdg/kdeglobals --group General --key DarkColorScheme 'BlossomUI Dark'
 \$KWRITE --file /etc/xdg/plasmarc --group Theme --key name 'BlossomUI'
+\$KWRITE --file /etc/xdg/kwinrc --group Effect-blur --key BlurStrength 5
 \$KWRITE --file /etc/xdg/plasmarc --group Theme --key LightColorScheme 'BlossomUI Light'
 \$KWRITE --file /etc/xdg/plasmarc --group Theme --key DarkColorScheme 'BlossomUI Dark'
 \$KWRITE --file /etc/xdg/plasmarc --group Theme --key LightLookAndFeel 'org.blossomos.ui.light.desktop'
